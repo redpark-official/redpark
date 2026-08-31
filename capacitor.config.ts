1cca97b64dabe68a61e3b15e44843854f084998f
Red Park <zlatoslav.onchev@gmail.com>
2026-08-31 14:32:47 +0000

    Add iOS Capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.redpark.game',
+  appName: 'Red Park',
+  webDir: 'public'
+};
+
+export default config;
